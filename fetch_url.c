7f5ca8cdb20cffbc1dbf867645daa19f31739204
Mini12349 <118483480+Mini12349@users.noreply.github.com>
2024-04-24 11:35:08 -0400

    Update fetch_url.c

--- fetch_url.c
+++ fetch_url.c
@@ -8,11 +8,14 @@
 
 #define MAX_URL_LENGTH 1024
 
+// Structure to hold crawler parameters
 typedef struct {
     int max_depth;
 } CrawlerParams;
 
+// Function to handle received data from cURL.
 size_t write_data(void *ptr, size_t size, size_t nmemb, void *userdata) {
+    // Here you can process the received data if needed.
     return size * nmemb;
 }
 
@@ -26,6 +29,7 @@ void *fetch_url(void *arg) {
         return NULL;
     }
 
+    // Regular expression for extracting links from HTML content
     regex_t regex;
     const char *pattern = "<a\\s+href=\"([^\"]+)\"";
     if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
@@ -34,10 +38,36 @@ void *fetch_url(void *arg) {
         return NULL;
     }
 
+    // Set the write callback function
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 
-    // Here, you can perform the fetch_url logic without the queue implementation
-    printf("Fetching URL...\n");
+    // URL to fetch (replace google.com with the desired URL)
+    const char *url = "http://google.com";
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+
+    char response_buffer[4096];
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, response_buffer);
+
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "Error: cURL request failed: %s\n", curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+        return NULL;
+    }
+
+    regmatch_t matches[2];
+    const char *ptr = response_buffer;
+    while (regexec(&regex, ptr, 2, matches, 0) == 0) {
+        char extracted_url[MAX_URL_LENGTH];
+        int len = matches[1].rm_eo - matches[1].rm_so;
+        if (len < MAX_URL_LENGTH) {
+            strncpy(extracted_url, ptr + matches[1].rm_so, len);
+            extracted_url[len] = '\0';
+            printf("Extracted URL: %s\n", extracted_url);
+        }
+        ptr += matches[0].rm_eo;
+    }
 
     regfree(&regex);
     curl_easy_cleanup(curl);
@@ -46,7 +76,7 @@ void *fetch_url(void *arg) {
 }
 
 int main() {
-    // Example usage of fetch_url function
+    // Set up crawler parameters
     CrawlerParams params = { .max_depth = 3 }; // Example max_depth
     fetch_url(&params);
     return 0;
